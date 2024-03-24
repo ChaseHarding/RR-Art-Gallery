9f4dbe34d84c4fa7ec0e43ef72d136a7f34a9b96
ChaseHarding <chaseharding965@gmail.com>
2024-03-24 11:26:37 -0700

    finished steps 1 through 4

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import dataReducer from './features/dataSlice'
 import { logger } from './features/middleware'
+// hello
 
 export const store = configureStore({
     reducer: {
